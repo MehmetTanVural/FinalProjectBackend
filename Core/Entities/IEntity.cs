553805475c939d45e99a5f72f6ba32e84458f793
Mehmet Tan VURAL <tanvural.00@gmail.com>
2024-02-09 00:43:36 +0300

    Add project files.

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    //IEntity implement eden class veri tabanı tablosudur
+   public interface IEntity
+    {
+    }
+}
